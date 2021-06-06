b9bc434ba8dced12c21f14db8f6a869798ffa94d
PriyanshuAgarwal <agarwal.iet888@gmail.com>
2021-06-06 12:39:13 +0530

    Format code

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
-let containerArray = [1,2,3,4];
+let containerArray = [1, 2, 3, 4];
 
-const html = containerArray.map( item =>{
-    return `<div class="item-list">
+const html = containerArray.map(item => {
+   return `<div class="item-list">
        conatiner ${item}
-    </div>` ; 
-   });
-   
-   document.getElementById("add-btn").addEventListener("click", newElement);
+    </div>`;
+});
+
+document.getElementById("add-btn").addEventListener("click", newElement);
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
+list.addEventListener('click', function (ev) {
+   if (ev.target.tagName === 'LI') {
+      ev.target.classList.toggle('checked');
+   }
 }, false);
 
 // Create a new list item when clicking on the "Add" button
@@ -23,25 +23,25 @@ function newElement() {
    var t = document.createTextNode(inputValue);
    li.appendChild(t);
    if (inputValue === '') {
-     alert("You must write something!");
+      alert("You must write something!");
    } else {
-     document.getElementById("task-list").appendChild(li);
+      document.getElementById("task-list").appendChild(li);
    }
    document.getElementById("myInput").value = "";
- 
+
    var span = document.createElement("SPAN");
    var txt = document.createTextNode("\u00D7");
    span.className = "close";
    span.appendChild(txt);
    li.appendChild(span);
- 
+
    for (i = 0; i < close.length; i++) {
-     close[i].onclick = function() {
-       var div = this.parentElement;
-       div.style.display = "none";
-     }
+      close[i].onclick = function () {
+         var div = this.parentElement;
+         div.style.display = "none";
+      }
    }
- }
- //document.getElementById("ctn-list").innerHTML = html.join('');
+}
+//document.getElementById("ctn-list").innerHTML = html.join('');
 
 //dragula([single1]);
