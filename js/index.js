a7ab839f2eda1fc6a2631fd04b0d834ba602652b
PriyanshuAgarwal <agarwal.iet888@gmail.com>
2021-07-18 16:48:00 +0530

    Update completed module

--- js/index.js
+++ js/index.js
@@ -6,15 +6,20 @@ let addBtn = document.getElementById("add-btn"),
 init();
 
 function init() {
-    let list = document.querySelector('ul');
-
     textInput.addEventListener("keyup", keyUp);
     textInput.setAttribute("autocomplete", "off");
     addBtn.classList.add("disabled");
     addBtn.addEventListener("click", addNewTask);
-    list.addEventListener('click', clicklistItem , false);
+    initListclickEvent();
 
     moment.tz.setDefault("Asia/Calcutta");
+}
+
+function initListclickEvent() {
+    let list = document.querySelectorAll('ul');
+    list.forEach(element => {
+        element.addEventListener('click', clicklistItem , false);
+    });
 }
 
 function keyUp(ele) {
@@ -32,26 +37,28 @@ function keyUp(ele) {
 
  function addNewTask(inputValue) {
     let inputObj = createNewTaskByID(inputValue);
-    addNewTaskNode(inputObj, true);
+    addNewTaskNode(inputObj, "todo-task-list", true);
     addToTaskList(inputObj);
     textInput.value = "";
     addBtn.classList.add("disabled");
  }
 
  // Create a new list item when clicking on the "Add" button
-function addNewTaskNode(inputObj, setFocus) {
+function addNewTaskNode(inputObj, section, setFocus) {
     let li = document.createElement("li"),
     textSpan = document.createElement("SPAN"),
     txt = document.createTextNode(inputObj.value);
 
     textSpan.appendChild(txt);
     li.appendChild(textSpan);
-    document.getElementById("task-list").appendChild(li);
+    document.getElementById(section).appendChild(li);
   
     let closeSpan = document.createElement("SPAN"),
     closetxt = document.createTextNode("\u00D7");
     closeSpan.className = "close";
     closeSpan.appendChild(closetxt);
+    if (inputObj["completed"])
+        li.classList.add("checked");
     li.appendChild(closeSpan);
     li.id = inputObj.id;
  
@@ -68,10 +75,41 @@ function addNewTaskNode(inputObj, setFocus) {
 
  function clicklistItem(ev) {
     if (ev.target.tagName === 'LI') {
-       console.log(ev.target)
-       ev.target.classList.toggle('checked');
-
+       //console.log(ev.target)
+       let id = ev.target.getAttribute("id"),
+       task = getTaskByID(id);
+       if (ev.target.classList.contains("checked")) {
+            task["completed"] = false;
+            ev.target.classList.remove('checked');
+       } else {
+            task["completed"] = true;
+            ev.target.classList.add('checked');
+       }
+       renderList();
     }
+ }
+
+ function renderList() {
+     let todolist = [],
+     completedlist = [];
+     todolist = tasksList.filter((task) => !task.completed),
+     completedlist = tasksList.filter((task) => task.completed);
+     document.getElementById("todo-task-list").innerHTML = "";
+     document.getElementById("completed-task-list").innerHTML = "";
+     if (completedlist.length > 0) 
+        document.getElementById("completed-section").classList.remove("hidden");
+    else
+        document.getElementById("completed-section").classList.add("hidden");
+     for (let i=0; i< todolist.length; i++ ) {
+        addNewTaskNode(todolist[i], "todo-task-list", false);
+     }
+     for (let i=0; i< completedlist.length; i++ ) {
+        addNewTaskNode(completedlist[i], "completed-task-list", false);
+     }
+ }
+
+ function getTaskByID(id) {
+     return tasksList.find((task) => task.id === id);
  }
 
  function createNewTaskByID(taskValue) {
@@ -84,6 +122,6 @@ function addNewTaskNode(inputObj, setFocus) {
 
 function addToTaskList(taskObj) {
     tasksList.push(taskObj);
-    console.log(tasksList);
+    //console.log(tasksList);
     //setStorage(tasksList);
  }
